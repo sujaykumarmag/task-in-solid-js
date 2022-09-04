798b9853112181adaf880f23b275876307c11dce
Sujay <sujay@Sujays-MacBook-Air.local>
2022-09-04 20:18:12 +0530

    Task Completed

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import {
+  Menu,
+  MenuTrigger,
+  MenuContent,
+  MenuGroup,
+  MenuLabel,
+  MenuItem,
+  Button,
+  Center
+} from "@hope-ui/solid";
+
+
+function App() {
+  return (
+    <Center m="200px" >
+      <Menu>
+        <Menu>
+          {({ opened }) => (
+            <>
+              <MenuTrigger
+                as={Button}
+                variant="subtle"
+                colorScheme="neutral">
+                {opened() ? "Options" : "Option 1"}
+              </MenuTrigger>
+              <MenuContent>
+                <MenuItem>Option 2</MenuItem>
+                <MenuItem>Option 3</MenuItem>
+              </MenuContent>
+            </>
+          )}
+        </Menu>
+        
+        
+      </Menu>
+    </Center>
+  );
+}
+
+export default App;
